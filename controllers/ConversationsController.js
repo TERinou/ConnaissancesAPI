c2be2daac27be7aaad84a0f5c30ed65067ab92a0
C.Thomas <c.tom34270@gmail.com>
2021-03-11 12:37:34 +0100

    Ended complex inferences (issue #14)

--- controllers/ConversationsController.js
+++ controllers/ConversationsController.js
@@ -1,100 +1,15 @@
+// --- Module import
 const Brain = require('@terinou/brain');
 const Questions = require('../models/Questions');
 const Words = require('../models/Words');
+const DumpRelations = require('../models/DumpRelations');
 
-const INFERENCES_MAX_DEPTH = 5;
+// --- Global function
+const {to} = require("../lib/to");
 
-/**
- * Handle POST replies.
- * If the content is queried with an id it means user reply a previous question.
- * Else reply a question to ask the user.
- * @param req
- * @param res
- */
-exports.onPostReplies = async function (req, res) {
 
-	const {id, content} = req.body;
-
-	// content is null || undefined, return Bad Request.
-	if (!content) {
-		return res.status(400).json({
-			ok: false,
-			code: 'CO40001',
-			message: 'User reply undefined'
-		});
-	}
-
-	// TODO Stop using getKeyWord and get all information needed
-	// get the main keyword of the phrase
-	// const keyword = brain.getKeyWord(content);
-	// if (!keyword) {
-	// 	return res.status(404).json({
-	// 		ok: false,
-	// 		code: 'CO40403',
-	// 		message: 'No keyword found'
-	// 	});
-	// }
-
-	// Mocking example
-	const information = {
-		object: "carotte",
-		relationType: "r_isa",
-		relation: "aliment"
-	}
-
-	// id is null || undefined, reply user's question.
-	if (!id) {
-		let word = information.object;	// object copy for manipulation
-		let inferences = []; 				// store each inference we found
-		let depth = 0;		 				// actual depth
-
-		while (depth < INFERENCES_MAX_DEPTH) {
-			try {
-				// Retrieve word data
-				const wordData = await Words.findOne({word: word}).exec();
-
-				// No word found
-				if (!wordData) {
-					return res.status(404).json({
-						ok: false,
-						code: 'CO40402',
-						message: `No word found for ${word}`
-					});
-				}
-
-				// Go through each relation
-				for (let relation of wordData.relations) {
-					if (relation.type === information.relationType) {
-						if (relation.word === information.relation) {
-							return res.status(200).json({ok: true, information, inferences});
-						}
-						word = relation.word;
-						inferences.push(word);
-						break;
-					}
-				}
-
-				depth++;
-			} catch (err) {
-				console.error(err);
-				return res.status(500).send(err.message);
-			}
-		}
-
-		return res.status(404).json({
-			ok: false,
-			code: 'CO40404',
-			message: 'Cannot find the relation',
-			information,
-			inferences
-		})
-	}
-
-	// otherwise confirm we get his response.
-	else {
-		return res.status(200).json({ok: true});
-	}
-}
+// --- Global variable
+const INFERENCE_MAX_DEPTH = 5;
 
 
 /**
@@ -125,3 +40,172 @@ exports.onGetQuestion = function (req, res) {
 		});
 	});
 }
+
+
+/**
+ * Handle POST replies.
+ * If the content is queried with an id it means user reply a previous question.
+ * Else reply a question to ask the user.
+ * @param req
+ * @param res
+ */
+exports.onPostReplies = async function (req, res) {
+
+	const {id, content} = req.body;
+
+	// --- content is null || undefined, return Bad Request.
+	if (!content) {
+		return res.status(400).json({
+			ok: false,
+			code: 'CO40001',
+			message: 'User reply undefined'
+		});
+	}
+
+	// --- id is null || undefined, reply user's question.
+	if (!id) {
+
+		// --- Get content relation
+		const relation = await Brain.getRelation(content);
+
+		try {
+			// --- Try to found the relation
+			const inferences = await foundRelation(relation);
+
+			// --- No relation found
+			if (!inferences) {
+				return res.status(404).json({
+					ok: false,
+					code: 'CO40403',
+					message: 'Cannot find the relation',
+					information: relation
+				});
+			}
+
+			// --- We found something
+			else {
+				return res.status(200).json({
+					ok: true,
+					relation,
+					inferences: (inferences.length > 1) ? inferences : undefined  // inferences length == 0 means we instantly found the relation, so it doesn't need to be return
+				});
+			}
+		} catch (err) {
+			console.error(err);
+			return res.status(500).send(err.message);
+		}
+	}
+
+	// --- Otherwise confirm we get his response.
+	else {
+		return res.status(200).json({ok: true});
+	}
+}
+
+
+/**
+ * Check if we found the relation related word in inferences and return it.
+ * If not we return the list of useful inference we found. (inference with r_isa and relation.type relation).
+ * Return null if the word from the relation doesn't exist
+ * @param relation	The main relation to check
+ * @returns {Promise<null|[]|*>}
+ */
+async function checkInferences(relation) {
+
+	let inferences = [];
+
+	// --- Find the word in the database.
+	const [err, word] = await to(Words.findOne({word: relation.word}).exec());
+
+	// --- If an error occurred.
+	if (err) {
+		console.error(err);
+		return {ok: false, err: err};
+	}
+
+	// --- No word found.
+	if (!word) {
+		console.error(`[-]No word found for ${relation.word}`);
+		return {ok: false};
+	}
+
+	// --- Get all inferences in our word relations (r_isa and the relation type we looking for).
+	for (let wordRelation of word.relations) {
+		if (wordRelation.type === DumpRelations.r_isa || wordRelation.type === relation.type) {
+
+			// --- We found the related word.
+			if (relation.relatedTo === wordRelation.word && relation.type === wordRelation.type) {
+				return {
+					ok: true,
+					relation: {
+						word: relation.word,
+						type: wordRelation.type,
+						relatedTo: wordRelation.word
+					}
+				};
+			}
+
+			// --- add a new inference
+			inferences.push({word: relation.word, relation: wordRelation.type, relatedTo: wordRelation.word});
+		}
+	}
+
+	return {ok: true, inferences: inferences};
+}
+
+
+/**
+ * Try to found the requested relation by checking inferences.
+ * Return the path if the relation has been found, else return null
+ * @param relation 	The main relation to find
+ * @param depth		The depth of search
+ * @param path		The path we take during the travel
+ * @returns {Promise<null|*[]>}
+ */
+async function foundRelation(relation, depth = 0, path = []) {
+
+	// --- If too deep we stop, else we are on a new depth.
+	if (depth >= INFERENCE_MAX_DEPTH) {
+		return null;
+	} else {
+		depth++;
+	}
+
+	// --- Check inferences if we found the relation.
+	// If not, we get the new inferences to work with.
+	const res = await checkInferences(relation);
+
+	// --- If something happened.
+	if (!res.ok) {
+		return null;
+	}
+
+	// --- If related word has been found.
+	if (res.relation) {
+		path.push(res.relation);
+		return path;
+	}
+
+	// --- Else loop on the new inferences (if there is).
+	if (res.inferences && res.inferences.length > 0) {
+		for (let inference of res.inferences) {
+
+			// --- Add the inference to the path.
+			path.push(inference);
+
+			// --- The new relation only change the word to check, the type and the related word doesn't change.
+			const newRelation = Object.assign({}, relation, {word: inference.relatedTo});
+
+			// --- If relation is found, then return the path we take.
+			// Else we remove the last inference from the path.
+			if (await foundRelation(newRelation, depth, path)) {
+				return path;
+			} else {
+				path.pop();
+			}
+		}
+	}
+
+	return null;
+}
+
