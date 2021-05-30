a2c33c46c6a951dfcbb50e6a2c7213470c9feef3
C.Thomas <c.tom34270@gmail.com>
2021-05-30 10:01:01 +0200

    Use Brain to get the Dump relations

--- models/DumpRelations.js
+++ models/DumpRelations.js
@@ -1,15 +1,2 @@
-// const Brain = require('@terinou/brain');
-
-// module.exports = Object.freeze(Brain.listRelation());
-
-module.exports = Object.freeze({
-	r_isa: 'r_isa',
-	r_carac: 'r_carac',
-	r_lieu: 'r_lieu',
-	r_agent: 'r_agent',
-	r_own: 'r_own',
-	r_color: 'r_color',
-	r_but: 'r_but',
-	r_conseq: 'r_conseq',
-	r_make: 'r_make'
-});
+const Brain = require('@terinou/brain');
+module.exports = Object.freeze(Brain.listRelation());
--- models/Words.js
+++ models/Words.js
@@ -24,43 +24,43 @@ const WordSchema = new mongoose.Schema({
 });
 
 WordSchema.pre('findOne', async function () {
-    const {word} = this.getQuery();
-    const {$disableMiddleware, ...query} = this.getQuery();
-    this.setQuery(query);
-    if (query.word && !$disableMiddleware) return await addWordFromJDM(word);
+  const { word } = this.getQuery();
+  const { $disableMiddleware, ...query } = this.getQuery();
+  this.setQuery(query);
+  if (query.word && !$disableMiddleware) return await addWordFromJDM(word);
 });
 
 WordSchema.pre('validate', async function () {
-    return await addRelationsFromJDM(this);
+  return await addRelationsFromJDM(this);
 });
 
 const WordModel = mongoose.model('Words', WordSchema);
 
 async function getRelationsFromJDM(word) {
-    const relations = (await jeuxdemots.search(word)).getInRelations();
-    return relations.reduce((relations, relation) => {
-        if (Object.keys(DumpRelations).includes(relation.type)) {
-            relations.push({word: relation.name, type: relation.type});
-        }
+  const relations = (await jeuxdemots.search(word)).getInRelations();
+  return relations.reduce((relations, relation) => {
+    if (Object.keys(DumpRelations).includes(relation.type)) {
+      relations.push({ word: relation.name, type: relation.type });
+    }
 
-        return relations;
-    }, []);
+    return relations;
+  }, []);
 }
 
 async function addRelationsFromJDM(newWord) {
-    newWord.relations = await getRelationsFromJDM(newWord)
-    return newWord;
+  newWord.relations = await getRelationsFromJDM(newWord)
+  return newWord;
 }
 
 async function addWordFromJDM(word) {
-    if(!(await WordModel.findOne({word, $disableMiddleware: true}))) {
-        return WordModel.findOneAndUpdate({word}, {
-            $setOnInsert: {
-                word,
-                relations: await getRelationsFromJDM(word)
-            }
-        }, {upsert: true, new: true}).exec();
-    }
+  if (!(await WordModel.findOne({ word, $disableMiddleware: true }))) {
+    return WordModel.findOneAndUpdate({ word }, {
+      $setOnInsert: {
+        word,
+        relations: await getRelationsFromJDM(word)
+      }
+    }, { upsert: true, new: true }).exec();
+  }
 }
 
 module.exports = WordModel;
